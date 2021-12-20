aeb3dba4dbebe733be3753d49b1c83148c1d4c70
Rebecca <njokinyagah@gmail.com>
2021-12-20 03:11:53 +0300

    updated js code

--- JS/demo.js
+++ JS/demo.js
@@ -1,5 +1,5 @@
 const akanNameCalculator = (CC, YY, MM, DD, gender) => {
-    // BODMAS
+ 
     const calculationResult = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
     let wholeNumber = Number(calculationResult.toFixed())
 
@@ -8,9 +8,7 @@ const akanNameCalculator = (CC, YY, MM, DD, gender) => {
     const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     if (wholeNumber >= 7) {
-        // TODO - Check this later
-        // Extra work would be to randomize wholeNumber
-        // Using Math library
+     
         wholeNumber = wholeNumber - 1;
     }
 
@@ -23,33 +21,31 @@ const akanNameCalculator = (CC, YY, MM, DD, gender) => {
 
 
 const handleClick = () => {
-    // Step One - Retrieve refrence to HTML elements
+
     const birthDayBox = document.getElementById("birthDay")
     const birthMonthBox = document.getElementById("birthMonth")
     const birthYearBox = document.getElementById("birthYear")
     const birthGenderBox = document.getElementById("birthGender")
 
-    // Step Two - Retrieve input elements values
+    
     const birthDay = birthDayBox.value;
     const birthMonth = birthMonthBox.value;
     const birthYear = birthYearBox.value;
     const birthGender = birthGenderBox.value;
 
-    // Check user gender
+    
     const allGendersArray = ["Male", "Female"]
     if (allGendersArray.includes(birthGender)) {
-        // CC = Century
-        // YY = Year
-        // 1989
+      
         const centuryVar = String(birthYear).substring(0, 2)
         const yearVar = String(birthYear).substring(2)
-            // Check that day and month are valid
+           
         if (birthDay <= 0 || birthDay > 31) {
             alert('Please enter a valid Day')
         } else if (birthMonth <= 0 || birthMonth > 12) {
             alert('Please enter a valid month')
         } else {
-            // Execute calculation
+         
             const akanName = akanNameCalculator(centuryVar, yearVar, birthMonth, birthDay, birthGender);
             alert(`Your Akan name is ${akanName.name} born on ${akanName.day}`)
         }
